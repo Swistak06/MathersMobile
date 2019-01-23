2e0b346c0dacfe7448001b25cf31ac0eff855ded
Łukasz Matysik <lukasz.matysik6@gmail.com>
2019-01-23 11:13:58 +0100

    Timer and generator

--- app/src/main/java/com/example/mathersmobile/GameFragment.kt
+++ app/src/main/java/com/example/mathersmobile/GameFragment.kt
@@ -2,14 +2,18 @@ package com.example.mathersmobile
 
 import android.content.Context
 import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_game.*
 import kotlinx.android.synthetic.main.fragment_game.view.*
 
 class GameFragment : Fragment() {
@@ -17,9 +21,14 @@ class GameFragment : Fragment() {
     private var min = 2
     private var max = 4
     private var size = 4
+    val handler = Handler()
+    var stopwatch = StopWatch(0,0,0,0)
+    var numberGenerator = NumberGenerator()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 //        arguments?.let { }
     }
 
@@ -31,8 +40,9 @@ class GameFragment : Fragment() {
         generateButtons(view)
         view.backBtn.setOnClickListener {
             //resetGameArea()
+            numberGenerator.createBoard(5,2,4)
             listener?.backToMenuListener()
-            //TODO: reset and stop the timer
+            resetTimer()
 
         }
         return view
@@ -54,6 +64,16 @@ class GameFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         listener = null
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startTimer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopTimer()
     }
 
     fun setMinMax(min:Int, max:Int){
@@ -132,7 +152,63 @@ class GameFragment : Fragment() {
         }
     }
 
+    val runningStopWatch = object: Runnable {
+        override fun run() {
+            addOneSecond()
+            handler.postDelayed(this, 1000)
+        }
+    }
 
+    fun startTimer(){
+        handler.removeCallbacks(runningStopWatch)
+        handler.post(runningStopWatch)
+    }
+    fun stopTimer(){
+        handler.removeCallbacks(runningStopWatch)
+    }
+    fun resetTimer(){
+        handler.removeCallbacks(runningStopWatch)
+        stopwatch = StopWatch(0,0,0,0)
+        secondsView.text = "0"
+        tenSecondsView.text = "0"
+        minutesView.text = "0"
+        tenMinutesView.text = "0"
+    }
+    fun addOneSecond(){
+        stopwatch.seconds++
+        if(stopwatch.seconds > 9){
+            secondsView.text = "0"
+            stopwatch.seconds = 0
+            addTenSeconds()
+        }
+        secondsView.text = stopwatch.seconds.toString()
+    }
+    fun addTenSeconds(){
+        stopwatch.tenSeconds++
+        if(stopwatch.tenSeconds>5){
+            tenSecondsView.text = "0"
+            stopwatch.tenSeconds = 0
+            addOneMinute()
+        }
+        tenSecondsView.text = stopwatch.tenSeconds.toString()
+
+
+    }
+    fun addOneMinute(){
+        stopwatch.minutes++
+        if(stopwatch.minutes>9){
+            minutesView.text = "0"
+            stopwatch.minutes = 0
+            addTenMinutes()
+        }
+        minutesView.text = stopwatch.minutes.toString()
+    }
+    fun addTenMinutes(){
+        if(stopwatch.tenMinutes<9){
+            stopwatch.tenMinutes++
+            tenMinutesView.text = stopwatch.tenMinutes.toString()
+        }
+    }
     interface GameFragmentListener {
         fun backToMenuListener()
     }
