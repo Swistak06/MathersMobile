a117c9d40907eb52b6b41b5b0bad3b766a07a19c
Kamil Drewniak <zibix12@gmail.com>
2019-01-23 11:10:53 +0100

    Clickable buttons

--- app/src/main/java/com/example/mathersmobile/GameFragment.kt
+++ app/src/main/java/com/example/mathersmobile/GameFragment.kt
@@ -18,6 +18,10 @@ class GameFragment : Fragment() {
     private var max = 4
     private var size = 4
 
+    private val leftGameSums : MutableList<GameSum> = mutableListOf()
+    private val topGameSums : MutableList<GameSum> = mutableListOf()
+    private val gameButtons : MutableList<GameButton> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        arguments?.let { }
@@ -65,74 +69,56 @@ class GameFragment : Fragment() {
         this.size = size
     }
     fun generateButtons(view: View){
-        var ids = 100
+        clearLayouts()
         val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f)
         for(i in 1..size){
             val horizLay = LinearLayout(context)
             horizLay.orientation = LinearLayout.HORIZONTAL
             horizLay.layoutParams = params
-            horizLay.id = ids + i*10
             for(j in 1..size){
-//
-//                val button = RelativeLayout(context)
-//                button.layoutParams = params
-//
-//                val img = ImageView(context)
-//                img.layoutParams = params
-//                img.setImageResource(R.drawable.selected70)
-//
-//                val textV = TextView(context)
-//                textV.layoutParams = params
-//                textV.gravity = Gravity.CENTER
-//                textV.text = "99"
-//                textV.textSize = 30f
-//                textV.setTextColor(Color.parseColor("#ffffff"))
-//
-//                button.addView(img)
-//                button.addView(textV)
-                val gameButton = GameButton(context, "99")
+                val gameButton = GameButton(this, 10, j-1, i-1)
+                gameButtons.add(gameButton)
                 horizLay.addView(gameButton)
             }
             view.linearLayout.addView(horizLay)
         }
         for(i in 1..size){
-            val leftB = RelativeLayout(context)
-            leftB.layoutParams = params
+            val leftGameSum = GameSum(context, 10, i-1)
+            leftGameSums.add(leftGameSum)
+            view.leftSumLayout.addView(leftGameSum)
 
-            var img = ImageView(context)
-            img.layoutParams = params
-            img.setImageResource(R.drawable.selected70)
-
-            var textV = TextView(context)
-            textV.layoutParams = params
-            textV.gravity = Gravity.CENTER
-            textV.text = "99"
-            textV.textSize = 30f
-            textV.setTextColor(Color.parseColor("#ffffff"))
-            leftB.addView(img)
-            leftB.addView(textV)
-            view.leftSumLayout.addView(leftB)
-
-            val topB = RelativeLayout(context)
-            topB.layoutParams = params
-
-            img = ImageView(context)
-            img.layoutParams = params
-            img.setImageResource(R.drawable.selected70)
-
-            textV = TextView(context)
-            textV.layoutParams = params
-            textV.gravity = Gravity.CENTER
-            textV.text = "99"
-            textV.textSize = 30f
-            textV.setTextColor(Color.parseColor("#ffffff"))
-            topB.addView(img)
-            topB.addView(textV)
-            view.topSumsLayout.addView(topB)
+            val topGameSum = GameSum(context, 10, i-1)
+            topGameSums.add(topGameSum)
+            view.topSumsLayout.addView(topGameSum)
         }
     }
+    fun clearLayouts(){
+        leftGameSums.clear()
+        topGameSums.clear()
+        gameButtons.clear()
+    }
+    fun changeGameSum(column:Int, row:Int, value:Int){
+        val leftSum = findLeftSum(row)
+        val topSum = findTopSum(column)
 
+        leftSum?.changeValue(value)
+        topSum?.changeValue(value)
 
+    }
+    private fun findLeftSum(row:Int) : GameSum?{
+        leftGameSums.forEach {
+            if(it.index == row)
+                return it
+        }
+        return null
+    }
+    private fun findTopSum(column:Int) : GameSum?{
+        topGameSums.forEach {
+            if(it.index == column)
+                return it
+        }
+        return null
+    }
     interface GameFragmentListener {
         fun backToMenuListener()
     }
---
+++ app/src/main/java/com/example/mathersmobile/GameSum.kt
@@ -0,0 +1,61 @@
+package com.example.mathersmobile
+
+import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+
+class GameSum(ctx: Context?, goal: Int, val index: Int) : RelativeLayout(ctx) {
+    private var textView: TextView? = null
+    private var imageView: ImageView? = null
+    var currentSum = 0
+    private var goalSum = goal
+    private var completed = false
+
+    init {
+        textView = TextView(ctx)
+        imageView = ImageView(ctx)
+
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f)
+        layoutParams = params
+
+        val textStr = currentSum.toString() + " / " + goalSum.toString()
+        textView?.text = textStr
+        textView?.layoutParams = params
+        textView?.gravity = Gravity.CENTER
+        textView?.textSize = 15f
+        textView?.setTextColor(Color.parseColor("#ffffff"))
+
+        imageView?.layoutParams = params
+        imageView?.setImageResource(R.drawable.uncompleted)
+
+        addView(imageView)
+        addView(textView)
+    }
+    private fun checkCompleted(){
+        if(currentSum == goalSum){
+            if(!completed) {
+                completed = true
+                imageView?.setImageResource(R.drawable.completed)
+            }
+        }
+        else{
+            if(completed){
+                completed = false
+                imageView?.setImageResource(R.drawable.uncompleted)
+            }
+        }
+    }
+    fun changeValue(value:Int){
+        currentSum += value
+        val textStr = currentSum.toString() + " / " + goalSum.toString()
+        textView?.text = textStr
+
+        checkCompleted()
+    }
+
+
+}
